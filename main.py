fde4cfe9edd08325f0ea32ef858d05c9f54417f5
Maziar Bastani <maziarbastani@gmail.com>
2021-12-15 23:35:02 -0800

    Delete post added

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from typing import ContextManager, Optional
-from fastapi import FastAPI
-from fastapi.params import Body
+from fastapi import FastAPI,status, HTTPException
 from pydantic import BaseModel
 from random import randrange
-from fastapi import status
 
 from starlette.responses import Response
+
 app = FastAPI()
 
 class Post(BaseModel):
@@ -21,6 +20,11 @@ def find_post(id):
         if p["id"]==id:
             return p
 
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id']==id:
+            return i
+
 @app.get("/")
 async def root():
     return{"message":"Hello World"}
@@ -30,7 +34,7 @@ async def get_posts():
     return{"data":my_posts}
 
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
     post_dict = post.dict()
     post_dict['id']= randrange(0,100000)
@@ -38,16 +42,20 @@ def create_post(post: Post):
     return{"data":post_dict}
 
 @app.get("/posts/{id}")
-def get_post(id:int,response:Response):
+def get_post(id:int):
     post = find_post(id)
     if not post:
-        response.status_code= status.HTTP_404_NOT_FOUND
-        return {"message":f"post with id {id} was not found"}
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} was not found")
     return{"post_deatil":post}
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id:int):
+    #deleting the post
+    index= find_index_post(id)
+    if index==None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id {id} not found")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-
-##@app.post("/createposts")
-##def create_post(payLoad:dict=Body(...)):
-##   print(payLoad)
-##   return {"new_post":f"title {payLoad['title']} Content:{payLoad['content']}"}
+@app.put("/post/{id}", status_code=status.)
+def update_post(id:int):
