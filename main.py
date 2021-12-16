3d1a573c93b7765d9cc84092632ae0dd1bff0b70
Maziar Bastani <maziarbastani@gmail.com>
2021-12-15 21:27:45 -0800

    404 HTTP Error code added

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ from typing import ContextManager, Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
+from fastapi import status
 
+from starlette.responses import Response
 app = FastAPI()
 
 class Post(BaseModel):
@@ -13,20 +16,36 @@ class Post(BaseModel):
 
 my_posts = [{"title":"title of post 1","content":"content of post 1","id":1},{"title":"best foods","content":"I like Pizza","id":2}]
 
+def find_post(id):
+    for p in my_posts:
+        if p["id"]==id:
+            return p
+
 @app.get("/")
 async def root():
     return{"message":"Hello World"}
 
 @app.get("/posts")
-async def get_post():
-    return{"data":"This is a post"}
+async def get_posts():
+    return{"data":my_posts}
 
 
 @app.post("/posts")
 def create_post(post: Post):
-    print(post)
-    print(post.dict())
-    return{"data":post}
+    post_dict = post.dict()
+    post_dict['id']= randrange(0,100000)
+    my_posts.append(post_dict)
+    return{"data":post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id:int,response:Response):
+    post = find_post(id)
+    if not post:
+        response.status_code= status.HTTP_404_NOT_FOUND
+        return {"message":f"post with id {id} was not found"}
+    return{"post_deatil":post}
+
+
 
 ##@app.post("/createposts")
 ##def create_post(payLoad:dict=Body(...)):
